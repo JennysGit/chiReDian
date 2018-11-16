f2d96046c010753d5339ad0dccc1cd9c554aed0d
SF\01376213 <jingzhang21@sfmail.sf-express.com>
2018-11-16 18:38:00 +0800

    repository init

---
+++ front/src/components/Deposit/Deposit.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+    ddd
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Deposit',
+  data () {
+    return {}
+  },
+  created () {
+    this.$axios.post('/confirm/lifeCycle')
+      .then((res) => {
+        if (res.code === 200) {
+          console.log('xxx')
+        }
+      })
+      .catch((err) => {
+      })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
